9cc05cbc03722a535ac3ef46a884f75f1964cc1f
dddddddnut? <90821101+26lennom@users.noreply.github.com>
2024-02-14 13:27:59 +0800

    Initial set up

---
+++ Objects/Ship.py
@@ -0,0 +1,32 @@
+from GameFrame import RoomObject, Globals
+import pygame
+
+class Ship(RoomObject):
+    #class for the player's avatar
+
+    def __init__(self, room,x ,y):
+
+        RoomObject.__init__(self, room, x, y)
+
+        image = self.load_room(Ship.png)
+        self.set_image(image,100,100)
+
+        self.handle_key_events = True
+
+    def key_pressed(self, key):
+        
+        if key[pygame.K_w]:
+            self.y_speed -= 10
+        elif key[pygame.K_s]:
+            self.y_speed += 10
+
+
+    def keep_in_room(self):
+
+        if self.y <0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+
+    def step(self):
+        self.keep_in_room()
---
+++ Objects/Title.py
@@ -0,0 +1,18 @@
+from GameFrame import RoomObject
+import pygame
+
+class Title(RoomObject):
+
+    def __init__(self, room, x, y):
+        RoomObject.__init__(self, room, x, y)
+
+        image = self.load_image('Title.png')
+        self.set_image(image,800,350)
+
+        self.handle_key_events = True
+
+    def key_pressed(self, key):
+        #return super().key_pressed(key)
+
+        if key[pygame.K_SPACE]:
+            self.room.running = False
